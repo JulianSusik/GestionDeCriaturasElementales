98e7e913e3cb9910dc037db96b0e6fec4055cc2e
JulianSusik <126615031+JulianSusik@users.noreply.github.com>
2025-11-20 18:49:04 -0300

    commit de criaturaancestral.test

--- test/ar/edu/unlam/pb2/gestiondecriaturaselementales/CriaturaAncestralTest.java
+++ test/ar/edu/unlam/pb2/gestiondecriaturaselementales/CriaturaAncestralTest.java
@@ -25,10 +25,33 @@ public class CriaturaAncestralTest {
 	@After
 	public void tearDown() throws Exception {
 	}
+	
+    @Test
+    public void sePuedeCrearUnaCriaturaAncestralYVerSusParametros() {
+        CriaturaElemental ance = new CriaturaAncestral("Dragon", Elementos.FUEGO, 180, EstadoEmocional.INESTABLE);
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        assertEquals("Dragon", ance.getNombre());
+        assertEquals(Elementos.FUEGO, ance.getAfinidadElemental());
+        assertEquals(Integer.valueOf(180), ance.getNivelEnergia());
+        assertEquals(EstadoEmocional.INESTABLE, ance.getEstado());
+    }
 
+
+    @Test
+    public void sePuedeEntrenarUnaCriaturaAncestral () throws CriaturaYaExistenteException {
+        CriaturaElemental ance = new CriaturaAncestral("Dragon", Elementos.FUEGO, 100, EstadoEmocional.INESTABLE);
+        ance.entrenar(50);
+        assertEquals(Integer.valueOf(150), ance.getNivelEnergia());
+    }
+
+    @Test (expected=LimiteEnergiaSuperadoException.class)
+    public void noSePuedeEntrenarMasAllaDelNivel200DeEnergia () throws LimiteEnergiaSuperadoException {
+        CriaturaElemental ance = new CriaturaAncestral("Dragon", Elementos.FUEGO, 150, EstadoEmocional.INESTABLE);
+        ance.entrenar(51);
+    }
+
+    @Test (expected=IllegalArgumentException.class)
+    public void laEnergiaDeLaCriaturaAncestralNoPuedeSerMenorA100 (){
+        CriaturaElemental ance = new CriaturaAncestral("Dragon", Elementos.FUEGO, 90, EstadoEmocional.INESTABLE);
+    }
 }
