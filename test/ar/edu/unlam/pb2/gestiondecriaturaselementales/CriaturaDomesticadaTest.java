1e0797a3702e1880c2a528160504086005115b64
LucccasMaidana <lucasmaidana.nhn@gmail.com>
2025-11-20 20:07:40 -0300

    Add tests for CriaturaDomesticada class

--- test/ar/edu/unlam/pb2/gestiondecriaturaselementales/CriaturaDomesticadaTest.java
+++ test/ar/edu/unlam/pb2/gestiondecriaturaselementales/CriaturaDomesticadaTest.java
@@ -2,33 +2,50 @@ package ar.edu.unlam.pb2.gestiondecriaturaselementales;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CriaturaDomesticadaTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @Test
+    public void queSePuedaCrearUnaCriaturaDomesticadaCorrectamente() {
+        CriaturaElemental dom = new CriaturaDomesticada("Felix", Elementos.AIRE, 120, EstadoEmocional.TRANQUILA);
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+        assertEquals("Felix", dom.getNombre());
+        assertEquals(Elementos.AIRE, dom.getAfinidadElemental());
+        assertEquals(Integer.valueOf(120), dom.getNivelEnergia());
+        assertEquals(EstadoEmocional.TRANQUILA, dom.getEstado());
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Test
+    public void queSePuedaEntrenarUnaCriaturaDomesticadaDeFormaEstable() throws LimiteEnergiaSuperadoException {
+        CriaturaElemental dom = new CriaturaDomesticada("Felix", Elementos.AIRE, 100, EstadoEmocional.TRANQUILA);
 
-	@After
-	public void tearDown() throws Exception {
-	}
+        dom.entrenar(30);
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        assertEquals(Integer.valueOf(130), dom.getNivelEnergia());
+    }
+
+    @Test(expected = LimiteEnergiaSuperadoException.class)
+    public void queNoSePuedaSuperarElLimiteDeEnergia() throws LimiteEnergiaSuperadoException {
+        CriaturaElemental dom = new CriaturaDomesticada("Felix", Elementos.AIRE, 190, EstadoEmocional.TRANQUILA);
+
+        dom.entrenar(20); 
+    }
+
+    @Test
+    public void queNuncaSeVuelvaInestable() {
+        CriaturaElemental dom = new CriaturaDomesticada("Felix", Elementos.AIRE, 160, EstadoEmocional.TRANQUILA);
+
+
+        assertNotEquals(EstadoEmocional.INESTABLE, dom.getEstado());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void queNoSePuedaCrearDomesticadaConEstadoInestable() {
+        new CriaturaDomesticada("Felix", Elementos.FUEGO, 140, EstadoEmocional.INESTABLE // → NO PERMITIDO
+        );
+    }
 
 }
+
+
