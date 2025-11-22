4d297a66661cd4394503a3cf6b6a96fe71553c04
LucccasMaidana <lucasmaidana.nhn@gmail.com>
2025-11-22 00:04:34 -0300

    Add CriaturaYaPacificaException class

---
+++ src/ar/edu/unlam/pb2/gestiondecriaturaselementales/CriaturaYaPacificaException.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.pb2.gestiondecriaturaselementales;
+
+public class CriaturaYaPacificaException extends RuntimeException {
+
+	public CriaturaYaPacificaException(String mensaje) {
+		// TODO Auto-generated constructor stub
+		super(mensaje);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
